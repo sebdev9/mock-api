d3a7d86a76126446a137031b193e61b366f11d79
sebastiencarpin <sebastien.carpin.ext@orangebank.com>
2025-02-20 17:06:08 +0100

    Moch files

---
+++ create_dirs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir -p /Users/sebastiencarpin/Documents/laguild/polites/mock-backend/survey/survey_1/question/1/
+mkdir -p /Users/sebastiencarpin/Documents/laguild/polites/mock-backend/survey/survey_3/question/3/
+mkdir -p /Users/sebastiencarpin/Documents/laguild/polites/mock-backend/survey/survey_5/question/5/
+mkdir -p /Users/sebastiencarpin/Documents/laguild/polites/mock-backend/survey/1713374910001/question/1/
+mkdir -p /Users/sebastiencarpin/Documents/laguild/polites/mock-backend/survey/survey_4/question/4/
+mkdir -p /Users/sebastiencarpin/Documents/laguild/polites/mock-backend/survey/survey_48/question/4/
+mkdir -p /Users/sebastiencarpin/Documents/laguild/polites/mock-backend/survey/1713374910002/question/2/
+mkdir -p /Users/sebastiencarpin/Documents/laguild/polites/mock-backend/survey/1713374910003/question/3/
+mkdir -p /Users/sebastiencarpin/Documents/laguild/polites/mock-backend/survey/survey_2/question/2/
+mkdir -p /Users/sebastiencarpin/Documents/laguild/polites/mock-backend/survey/1713374910004/question/4/
+mkdir -p /Users/sebastiencarpin/Documents/laguild/polites/mock-backend/survey/survey_6/question/6/
+mkdir -p /Users/sebastiencarpin/Documents/laguild/polites/mock-backend/survey/1713374910005/question/5/
+mkdir -p /Users/sebastiencarpin/Documents/laguild/polites/mock-backend/survey/survey_7/question/7/
+mkdir -p /Users/sebastiencarpin/Documents/laguild/polites/mock-backend/survey/survey_8/question/8/
+mkdir -p /Users/sebastiencarpin/Documents/laguild/polites/mock-backend/survey/survey_9/question/9/
+mkdir -p /Users/sebastiencarpin/Documents/laguild/polites/mock-backend/survey/survey_10/question/10/
